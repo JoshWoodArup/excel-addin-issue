6088f0cb8659072ecb561e54ceb20e2cb09d718e
Josh Wood <Josh.Wood@arup.com>
2024-10-16 15:22:08 +0100

    Initial commit

---
+++ src/taskpane/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+
+/* global document, Office, HTMLElement */
+
+const title = "Contoso Task Pane Add-in";
+
+const rootElement: HTMLElement | null = document.getElementById("container");
+const root = rootElement ? createRoot(rootElement) : undefined;
+
+/* Render application after Office initializes */
+Office.onReady(() => {
+  root?.render(
+    <FluentProvider theme={webLightTheme}>
+      <h2>{title}</h2>
+    </FluentProvider>
+  );
+});
